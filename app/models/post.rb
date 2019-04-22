9d5e78a79b388640a1f6c2a60d899d48ea638355
yukijina <yukijina@gmail.com>
2019-04-22 06:37:52 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 30 }
+  validates :category, inclusion: { in: %w(Non-Fiction, Fiction)}
+  validate :non_clickbait
+
+  def non_clickbait
+    if self.title && !self.title.include?("Won't Believe")
+      errors.add(:title, "wrong title")
+    end
+  end
 end
