50b4821305c9de78fce8c06f2d08f290658aa69f
Michael Schiumo <mschiumo18@gmail.com>
2019-04-23 13:28:13 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,19 @@ class Post < ActiveRecord::Base
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction, Non-fiction) }
   #validates :title, inclusion: { in: %w("Won't Believe", "Secret", "Top [number]", "Guess") }
-  validates :title, inclusion: { in: %w('Wont Believe', 'Secret', 'Top [number]', 'Guess'
-      :message => "%{value} does not include keywords") }
+  validates :is_clickbait?
+
+    CLICKBAIT_PATTERNS = [
+      /Won't Believe/i,
+      /Secret/i,
+      /Top [0-9]*/i,
+      /Guess/i
+      ]
+
+    def is_clickbait?
+      if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+        errors.add(:title, "must be clickbait")
+      end
+    end
+
 end
