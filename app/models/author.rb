ee1cda607314476223b2d9060d0d6ad11424b19b
norawolf <noraevogt@gmail.com>
2019-04-29 12:16:53 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ActiveRecord::Base
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates_length_of :phone_number, is: 10
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates_length_of :content, minimum: 250
+  validates_length_of :summary, maximum: 250
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :is_clickbait?
+
+  def is_clickbait?
+    clickbait = [/Won't Believe/i, /Secret/i, /Top \d+/i, /Guess/i]
+    if clickbait.none?{|x| x.match self.title}
+      errors.add(:title, "title must be clickbait")
+    end
+  end
 end
